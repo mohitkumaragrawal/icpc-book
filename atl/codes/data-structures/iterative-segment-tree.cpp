3422aab3e05d85f775ec29a4d84cf32160ddbbdc
Mohit Agrawal <mhtkrag@gmail.com>
2023-12-19 16:43:37 +0530

    changes

--- atl/codes/data-structures/iterative-segment-tree.cpp
+++ atl/codes/data-structures/iterative-segment-tree.cpp
@@ -18,3 +18,12 @@ int query(int l, int r) {  // sum on interval [l, r)
   }
   return res;
 }
+
+int main() {
+  scanf("%d", &n);
+  for (int i = 0; i < n; ++i) scanf("%d", t + n + i);
+  build();
+  modify(0, 1);
+  printf("%d\n", query(3, 11));
+  return 0;
+}
